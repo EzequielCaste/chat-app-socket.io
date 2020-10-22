21335b27adb34e6cd0d9e64f8e9b4121df0e6cd9
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-22 17:41:05 -0300

    Add Server side events

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.use( express.static('public') );
+
+io.sockets.on('connection', (socket) => {
+
+  socket.on('username', (username) => {
+    socket.username = username;
+    io.emit('is_online', `🔵 <i>${socket.username} joined the chat.</i>`)
+  });
+
+  socket.on('disconnect', (username) => {    
+    io.emit('is_online', `🔴 <i>${socket.username} left the chat.</i>`)
+  })
+
+  socket.on('is_typing', (username) => {
+    socket.broadcast.emit('is_typing', `<i>${socket.username} is typing</i>`)
+  })
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', `<strong>${socket.username}</strong>: ${msg}`);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
